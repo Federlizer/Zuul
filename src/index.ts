60deb04617e5a137a7f03a46d1d910ea3a449cf0
Federlizer <federlizer@gmail.com>
2019-07-19 13:43:09 +0200

    Original map implemented

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import uuid from 'uuid/v4';
-
 import Player from './models/Player';
-import Entity from './models/Entity';
 import Battle from './battle/Battle';
-import Room from './models/Room';
-import Item, { ItemType } from './models/Item';
 
 import commander from './commander';
-import Enemy from './models/Enemy';
+
+import map from './maps/original';
 
 // TODO: use inputFunction and outputFunction for I/O
 // Check it when the game get's initialized and then pass around the funtions that are to be used.
@@ -15,65 +11,50 @@ import Enemy from './models/Enemy';
 
 class Game {
     player: Player;
+    EXIT_GAME_VALUES: Array<string> = ['exit', 'quit', 'q'];
+    turnsTaken: number;
 
     constructor() {
-        const entrance = new Room('Entrance', 'The entrance of the building.');
-        const bathroom = new Room('Bathroom', 'A simple bathroom with a sink and a toilet');
-        const arena = new Room('Arena', 'An arena filled with a crowd and an enemy waiting for you at the other end.');
-
-        const sword = new Item(uuid(), 'Sword', 10, true, ItemType.Weapon);
-        const armor = new Item(uuid(), 'Armor', 10, true, ItemType.Armor);
-        bathroom.addItems(sword, armor);
-
-        entrance.addExit({
-            direction: 'west',
-            room: bathroom,
-        })
-
-        bathroom.addExit({
-            direction: 'east',
-            room: entrance,
-        })
-        bathroom.addExit({
-            direction: 'west',
-            room: arena,
-        })
-
-        arena.addExit({
-            direction: 'east',
-            room: bathroom,
-        })
-
-        const player = new Player('Federlizer', 100, 5, entrance);
-        const enemy = new Enemy('Enemy', 100, 5, arena);
-
-        this.player = player;
+        this.player = map.player;
+        this.turnsTaken = 0;
     }
 
     async start() {
-        while (true) {
-            const input = await this.player.getInput()
-            if (input === 'quit') {
+        // Introduce the map
+        console.log(map.intro);
+
+        // Main game loop
+        while (!map.isObjectiveCompleted()) {
+            if (map.turnToActivateEvent === this.turnsTaken) {
+                console.log(map.event());
+            }
+            
+            const input = await this.player.getInput(`${this.turnsTaken}> `);
+            
+            // Exit if user want's to stop playing
+            if (this.EXIT_GAME_VALUES.find((value) => input === value)) {
                 return;
             }
+            
             const command = commander.parse(input);
+            
             if (command !== null) {
                 commander.execute(command, this.player);
+
+                if (command.action === 'go') {
+                    const other = this.player.currentRoom.hasOtherEntity(this.player);
+                    if (other) {
+                        const battle = new Battle(this.player, other);
+                        await battle.startBattle();
+                    }
+                }
             } else {
                 console.log("Command unknown. Type help for help.");
             }
+
+            this.turnsTaken += 1;
         }
-    }
-
-    battle() {
-        const field: Room = new Room("Field", "The arena the two are to fight in");
-        let player: Entity = new Player("Federlizer", 50, 10, field);
-        let alien: Entity = new Enemy("Alien", 20, 5, field);
-
-        let battle: Battle = new Battle(player, alien);
-        battle.startBattle()
-            .then(() => console.log('The battle ended'))
-            .catch((err) => console.error(err));
+        console.log('Congratulations, you have completed all the objectives required from you. You can go home now. :)');
     }
 }
 
---
+++ src/maps/original.ts
@@ -0,0 +1,110 @@
+import Player from "../models/Player";
+import Room from "../models/Room";
+import Item, { ItemType } from "../models/Item";
+import uuid from 'uuid/v4';
+import Enemy from "../models/Enemy";
+
+// Rooms
+const entrance = new Room('Entrance', 'The entrance of the spaceship.');
+const escapePod = new Room('Escape Pod', 'Your goal is right in front of you. You see a shining, golden escape pod!');
+const storageRoom = new Room('Storage Room', 'The storage room is a simple storage room. You can see a bunch of crates full of food and supplies.');
+const airlock = new Room('Airlock', 'You are in the airlock. Nothing too interesting to see here');
+const lab = new Room('Lab', 'This is the laboratory on the ship. You can see a few medical stations around accompanied by a lot of chemical flasks.');
+const livingQuarters = new Room('Living Quarters', 'It is a simple room, filled with entertainment systems.');
+const controlCenter = new Room('Control Center', 'You are in the control center. There are a bunch of computers around');
+const lifeSupportCenter = new Room('Life Support Center', 'A simple life support room. A bunch of oxygen tanks, all running smoothly. For now...');
+const medbay = new Room('Medbay', 'This is the medical bay. You can see so many medical components.');
+
+// Room structure
+entrance.addExit({ direction: 'south', room: lifeSupportCenter });
+entrance.addExit({ direction: 'north', room: controlCenter });
+
+lifeSupportCenter.addExit({ direction: 'north', room: entrance });
+lifeSupportCenter.addExit({ direction: 'northwest', room: medbay });
+lifeSupportCenter.addExit({ direction: 'west', room: airlock });
+
+airlock.addExit({ direction: 'east', room: lifeSupportCenter });
+airlock.addExit({ direction: 'north', room: medbay });
+airlock.addExit({ direction: 'west', room: lab });
+
+lab.addExit({ direction: 'north', room: escapePod });
+lab.addExit({ direction: 'northeast', room: medbay });
+lab.addExit({ direction: 'east', room: airlock });
+
+escapePod.addExit({ direction: 'south', room: lab });
+
+storageRoom.addExit({ direction: 'southeast', room: medbay });
+storageRoom.addExit({ direction: 'east', room: livingQuarters });
+
+livingQuarters.addExit({ direction: 'west', room: storageRoom });
+livingQuarters.addExit({ direction: 'south', room: medbay });
+livingQuarters.addExit({ direction: 'east', room: controlCenter });
+
+controlCenter.addExit({ direction: 'south', room: entrance });
+controlCenter.addExit({ direction: 'southwest', room: medbay });
+controlCenter.addExit({ direction: 'west', room: livingQuarters });
+
+medbay.addExit({ direction: 'northwest', room: storageRoom });
+medbay.addExit({ direction: 'southwest', room: lab });
+medbay.addExit({ direction: 'north', room: livingQuarters });
+medbay.addExit({ direction: 'south', room: airlock });
+medbay.addExit({ direction: 'northeast', room: controlCenter });
+medbay.addExit({ direction: 'southeast', room: lifeSupportCenter });
+
+// Items
+const bandage = new Item(uuid(), 'Bandage', 25, false, ItemType.Healing);
+const medkit = new Item(uuid(), 'Medkit', 60, false, ItemType.Healing);
+const rifle = new Item(uuid(), 'Rifle', 12, true, ItemType.Weapon);
+const laserGun = new Item(uuid(), 'Laser Gun', 28, true, ItemType.Weapon);
+const nanoArmor = new Item(uuid(), 'Nano-armor', 20, true, ItemType.Armor);
+
+// Add items to rooms
+storageRoom.addItems(rifle);
+medbay.addItems(medkit);
+lifeSupportCenter.addItems(bandage);
+lab.addItems(laserGun, nanoArmor);
+
+// Entitties
+const player = new Player('Federlizer', 100, 10, entrance);
+let alienBoss: Enemy;
+let alien1: Enemy;
+let alien2: Enemy;
+
+// Break-in event
+function event(): string {
+  alienBoss = new Enemy('Alien Boss', 150, 15, escapePod);
+
+  alien1 = new Enemy('Alien 1', 50, 5, medbay);
+  alien2 = new Enemy('Alien 2', 50, 5, lab);
+
+  return `Three aliens just broke into the ship! Hurry!`;
+}
+
+// Function to check if the objectives are completed for the map
+function isObjectiveCompleted(): boolean {
+  if (!alienBoss) {
+    return false;
+  }
+
+  if ((alienBoss.currentHealth > 0) || (player.currentRoom.name !== escapePod.name)) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+// Intro to the map
+const intro = 'Welcome to Zuul. This is a text based game ' +
+  'where you must escape a space ship full of aliens that are looking for you, ' +
+  'because you stole something from them. Something very important. GET TO WORK!!!\n' +
+  'You find yourself in the entrance of the ship. ' +
+  'The pressure has just been stabalized so you can move on to any other room.'
+
+export default {
+  turnToActivateEvent: 4,
+  intro,
+  player,
+  startingRoom: entrance,
+  event,
+  isObjectiveCompleted,
+}
